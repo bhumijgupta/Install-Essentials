3250d4e794eb9f31e499cbd77389ea8b6b41bb16
bhumijgupta <bhumijgupta@gmail.com>
2020-01-27 14:37:36 +0530

    Initial Commit
    Signed-off-by: bhumijgupta <bhumijgupta@gmail.com>

---
+++ README.md
@@ -0,0 +1,42 @@
+# Install Essentials
+> Install Essentials is a sweet little bash script that install all the essentials I need to set up my new computer.
+
+It is a life saver and saves you from all the manual labour of installing the familiar software and setup your devenvironment.
+
+## Features
+* Easy to customise
+* Colored outputs
+* Choose to install packages/software you need
+* Saves time
+
+## Usage
+1. Download the script
+```bash
+wget
+```
+2. Run the script
+```bash
+bash essentials.sh
+```
+
+## License
+
+![GitHub](https://img.shields.io/github/license/bhumijgupta/huffman-compression-library)
+
+This library is licensed under MIT License. The license file can be found [here](LICENSE)
+
+## Author
+
+[![forthebadge](https://forthebadge.com/images/badges/built-with-love.svg)](https://forthebadge.com)
+
+### Bhumij Gupta
+
+![GitHub followers](https://img.shields.io/github/followers/bhumijgupta?label=Follow&style=social) [![LinkedIn](https://img.shields.io/static/v1.svg?label=connect&message=@bhumijgupta&color=success&logo=linkedin&style=flat&logoColor=white)](https://www.linkedin.com/in/bhumijgupta/) ![Twitter URL](https://img.shields.io/twitter/url?style=social&url=http%3A%2F%2Ftwitter.com%2Fbhumijgupta)
+
+---
+
+```C++
+if(repo.isAwesome || repo.isHelpful){
+    StarRepo();
+}
+```
---
+++ essentials.sh
@@ -0,0 +1,183 @@
+printf "\n\033[0;32mUpdating Package List\033[0m\n" 
+sudo apt update -y
+printf "\n\033[0;32mUpgrading Packages\033[0m\n"
+sudo apt upgrade -y
+
+read -r -p $'\nInstall git? [Y/n] ' input
+case $input in
+    [yY][eE][sS]|[yY])
+    printf "\n\033[0;32mInstalling git\033[0m\n"
+    sudo apt install git -y
+        ;;
+    [nN][oO]|[nN])
+    printf "\n\033[0;37m Skipping git\033[0m\n"
+       ;;
+    *)
+ printf "\n\033[0;31mInvalid input...\033[0m\n"
+ exit 1
+ ;;
+esac
+
+read -r -p $'\nInstall pip for python 3? [Y/n] ' input
+case $input in
+    [yY][eE][sS]|[yY])
+    printf "\n\033[0;32mInstalling pip3\033[0m\n"
+    sudo apt install python3-pip -y
+        ;;
+    [nN][oO]|[nN])
+    printf "\n\033[0;37m Skipping pip3\033[0m\n"
+       ;;
+    *)
+ printf "\n\033[0;31mInvalid input...\033[0m\n"
+ exit 1
+ ;;
+esac
+
+read -r -p $'\nInstall C/C++ compiler? [Y/n] ' input
+case $input in
+    [yY][eE][sS]|[yY])
+    printf "\n\033[0;32mInstalling C/C++ compiler\033[0m\n"    
+    sudo apt install build-essential manpages-dev -y
+        ;;
+    [nN][oO]|[nN])
+    printf "\n\033[0;37m Skipping C/C++ compiler\033[0m\n"
+       ;;
+    *)
+ printf "\n\033[0;31mInvalid input...\033[0m\n"
+ exit 1
+ ;;
+esac
+
+read -r -p $'\nInstall Chrome? [Y/n] ' input
+case $input in
+    [yY][eE][sS]|[yY])
+    printf "\n\033[0;32mInstalling Chrome\033[0m\n"    
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    sudo dpkg -i google-chrome-stable_current_amd64.deb
+    printf "\nCleaning up"
+    rm google-chrome-stable_current_amd64.deb
+        ;;
+    [nN][oO]|[nN])
+    printf "\n\033[0;37m Skipping Chrome\033[0m\n"
+       ;;
+    *)
+ printf "\n\033[0;31mInvalid input...\033[0m\n"
+ exit 1
+ ;;
+esac
+
+read -r -p $'\nInstall Spotify? [Y/n] ' input
+case $input in
+    [yY][eE][sS]|[yY])
+    printf "\n\033[0;32mInstalling Spotify\033[0m\n"    
+    sudo snap install spotify
+        ;;
+    [nN][oO]|[nN])
+    printf "\n\033[0;37m Skipping Spotify\033[0m\n"
+       ;;
+    *)
+ printf "\n\033[0;31mInvalid input...\033[0m\n"
+ exit 1
+ ;;
+esac
+
+read -r -p $'\nInstall VSCode? [Y/n] ' input
+case $input in
+    [yY][eE][sS]|[yY])
+    printf "\n\033[0;32mInstalling VSCode\033[0m\n"    
+    sudo snap install --classic code
+        ;;
+    [nN][oO]|[nN])
+    printf "\n\033[0;37m Skipping VSCode\033[0m\n"
+       ;;
+    *)
+ printf "\n\033[0;31mInvalid input...\033[0m\n"
+ exit 1
+ ;;
+esac
+
+read -r -p $'\nInstall Postman? [Y/n] ' input
+case $input in
+    [yY][eE][sS]|[yY])
+    printf "\n\033[0;32mInstalling Postman\033[0m\n"    
+    sudo snap install postman
+        ;;
+    [nN][oO]|[nN])
+    printf "\n\033[0;37m Skipping Postman\033[0m\n"
+       ;;
+    *)
+ printf "\n\033[0;31mInvalid input...\033[0m\n"
+ exit 1
+ ;;
+esac
+
+read -r -p $'\nInstall MongoDB? [Y/n] ' input
+case $input in
+    [yY][eE][sS]|[yY])
+    printf "\n\033[0;32mInstalling MongoDB\033[0m\n"    
+    sudo apt-get install gnupg -y
+    wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
+    printf "\ndeb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
+    sudo apt-get update
+    sudo apt-get install -y mongodb-org
+    printf "\nStarting MongoDB"
+    sudo service mongod start
+        ;;
+    [nN][oO]|[nN])
+    printf "\n\033[0;37m Skipping MongoDB\033[0m\n"
+       ;;
+    *)
+ printf "\n\033[0;31mInvalid input...\033[0m\n"
+ exit 1
+ ;;
+esac
+
+read -r -p $'\nInstall NodeJS? [Y/n] ' input
+case $input in
+    [yY][eE][sS]|[yY])
+    printf "\n\033[0;32mInstalling NodeJS\033[0m\n"    
+    sudo apt install curl -y
+    curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+    sudo apt install nodejs -y
+        ;;
+    [nN][oO]|[nN])
+    printf "\n\033[0;37m Skipping NodeJS\033[0m\n"
+       ;;
+    *)
+ printf "\n\033[0;31mInvalid input...\033[0m\n"
+ exit 1
+ ;;
+esac
+
+read -r -p $'\nInstall VLC? [Y/n] ' input
+case $input in
+    [yY][eE][sS]|[yY])
+    printf "\n\033[0;32mInstalling VLC\033[0m\n"    
+    sudo snap install vlc
+        ;;
+    [nN][oO]|[nN])
+    printf "\n\033[0;37m Skipping VLC\033[0m\n"
+       ;;
+    *)
+ printf "\n\033[0;31mInvalid input...\033[0m\n"
+ exit 1
+ ;;
+esac
+
+read -r -p $'\nInstall Terminator? [Y/n] ' input
+case $input in
+    [yY][eE][sS]|[yY])
+    printf "\n\033[0;32mInstalling Terminator\033[0m\n"    
+    sudo apt-get install terminator -y
+        ;;
+    [nN][oO]|[nN])
+    printf "\n\033[0;37m Skipping Terminator\033[0m\n"
+       ;;
+    *)
+ printf "\n\033[0;31mInvalid input...\033[0m\n"
+ exit 1
+ ;;
+esac
+# google-chrome https://www.mongodb.com/download-center/compass?jmp=docs
+
+
