df6afc700ce51c1ebd8a33a274831f00e8876eb1
bhumijgupta <bhumijgupta@gmail.com>
2020-11-14 23:36:58 +0530

    👌 IMPROVE: Refactor to global color variables
    Signed-off-by: bhumijgupta <bhumijgupta@gmail.com>

--- essentials.sh
+++ essentials.sh
@@ -16,15 +16,24 @@ APT_GET_DIR=$(which apt-get)
 sudo true
 
 # ***************
+# Colors 
+# ***************
+# TODO: Replace with https://stackoverflow.com/a/45122025/10207937
+GREEN="\033[0;32m"
+RED="\033[0;31m"
+GREY="\033[0;37m"
+NO_COLOR="\033[0m"
+
+# ***************
 # Check Compatibility 
 # ***************
-printf "\n\033[0;32mChecking Compatibility\033[0m\n"
+printf "\n${GREEN}Checking Compatibility${NO_COLOR}\n"
 if [ -z "$APT_GET_DIR" ]; then
-    printf "\033[0;31mCompatibility check failed. Cannot find apt-get\033[0m\n"
-    printf "\033[0;31mExiting\033[0m\n"
+    printf "${RED}Compatibility check failed. Cannot find apt-get${NO_COLOR}\n"
+    printf "${RED}Exiting${NO_COLOR}\n"
     exit 1
 else
-    printf "\033[0;32mCompatibilty check passed\033[0m\n"
+    printf "${GREEN}Compatibilty check passed${NO_COLOR}\n"
 fi
 
 # ***************
@@ -72,144 +81,144 @@ install_docker=${install_docker:-Y}
 read -r -p 'Install Docker Compose? [Y/n] ' install_docker_compose
 install_docker_compose=${install_docker_compose:-Y}
 
-printf "\n\033[0;32mUpdating Package List\033[0m\n" 
+printf "\n${GREEN}Updating Package List${NO_COLOR}\n" 
 sudo apt update -y
 
 case $upgrade_packages in
     [yY][eE][sS]|[yY])
-    printf "\n\033[0;32mUpgrading Packages\033[0m\n"
+    printf "\n${GREEN}Upgrading Packages${NO_COLOR}\n"
     sudo apt install git -y
         ;;
     [nN][oO]|[nN])
-    printf "\n\033[0;37m Skipping package updation\033[0m\n"
+    printf "\n${GREY} Skipping package updation${NO_COLOR}\n"
        ;;
     *)
- printf "\n\033[0;31mInvalid input...\033[0m\n"
+ printf "\n${RED}Invalid input...${NO_COLOR}\n"
  exit 1
  ;;
 esac
 
 case $install_git in
     [yY][eE][sS]|[yY])
-    printf "\n\033[0;32mInstalling git\033[0m\n"
+    printf "\n${GREEN}Installing git${NO_COLOR}\n"
     sudo apt install git -y
         ;;
     [nN][oO]|[nN])
-    printf "\n\033[0;37m Skipping git\033[0m\n"
+    printf "\n${GREY} Skipping git${NO_COLOR}\n"
        ;;
     *)
- printf "\n\033[0;31mInvalid input...\033[0m\n"
+ printf "\n${RED}Invalid input...${NO_COLOR}\n"
  exit 1
  ;;
 esac
 
 case $install_pip3 in
     [yY][eE][sS]|[yY])
-    printf "\n\033[0;32mInstalling pip3\033[0m\n"
+    printf "\n${GREEN}Installing pip3${NO_COLOR}\n"
     sudo apt install python3-pip -y
         ;;
     [nN][oO]|[nN])
-    printf "\n\033[0;37m Skipping pip3\033[0m\n"
+    printf "\n${GREY} Skipping pip3${NO_COLOR}\n"
        ;;
     *)
- printf "\n\033[0;31mInvalid input...\033[0m\n"
+ printf "\n${RED}Invalid input...${NO_COLOR}\n"
  exit 1
  ;;
 esac
 
 case $install_gcc in
     [yY][eE][sS]|[yY])
-    printf "\n\033[0;32mInstalling C/C++ compiler\033[0m\n"    
+    printf "\n${GREEN}Installing C/C++ compiler${NO_COLOR}\n"    
     sudo apt install build-essential manpages-dev -y
         ;;
     [nN][oO]|[nN])
-    printf "\n\033[0;37m Skipping C/C++ compiler\033[0m\n"
+    printf "\n${GREY} Skipping C/C++ compiler${NO_COLOR}\n"
        ;;
     *)
- printf "\n\033[0;31mInvalid input...\033[0m\n"
+ printf "\n${RED}Invalid input...${NO_COLOR}\n"
  exit 1
  ;;
 esac
 
 case $install_chrome in
     [yY][eE][sS]|[yY])
-    printf "\n\033[0;32mInstalling Chrome\033[0m\n"    
+    printf "\n${GREEN}Installing Chrome${NO_COLOR}\n"    
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
     sudo dpkg -i google-chrome-stable_current_amd64.deb
     printf "\nCleaning up"
     rm google-chrome-stable_current_amd64.deb
         ;;
     [nN][oO]|[nN])
-    printf "\n\033[0;37m Skipping Chrome\033[0m\n"
+    printf "\n${GREY} Skipping Chrome${NO_COLOR}\n"
        ;;
     *)
- printf "\n\033[0;31mInvalid input...\033[0m\n"
+ printf "\n${RED}Invalid input...${NO_COLOR}\n"
  exit 1
  ;;
 esac
 
 case $install_spotify in
     [yY][eE][sS]|[yY])
-    printf "\n\033[0;32mInstalling Spotify\033[0m\n"    
+    printf "\n${GREEN}Installing Spotify${NO_COLOR}\n"    
     sudo snap install spotify
         ;;
     [nN][oO]|[nN])
-    printf "\n\033[0;37m Skipping Spotify\033[0m\n"
+    printf "\n${GREY} Skipping Spotify${NO_COLOR}\n"
        ;;
     *)
- printf "\n\033[0;31mInvalid input...\033[0m\n"
+ printf "\n${RED}Invalid input...${NO_COLOR}\n"
  exit 1
  ;;
 esac
 
 case $install_code in
     [yY][eE][sS]|[yY])
-    printf "\n\033[0;32mInstalling VSCode\033[0m\n"    
+    printf "\n${GREEN}Installing VSCode${NO_COLOR}\n"    
     sudo snap install --classic code
     read -r -p $'\nInstall VSCode recommended extensions ? [Y/n] ' input
     input=${input:-Y}
     case $input in
         [yY][eE][sS]|[yY])
-        printf "\n\033[0;32mInstalling extensions\033[0m\n"    
+        printf "\n${GREEN}Installing extensions${NO_COLOR}\n"    
         code --install-extension christian-kohler.path-intellisense --user-data-dir="~/.vscode-root"
         code --install-extension CoenraadS.bracket-pair-colorizer --user-data-dir="~/.vscode-root"
         code --install-extension esbenp.prettier-vscode --user-data-dir="~/.vscode-root"
             ;;
         [nN][oO]|[nN])
-        printf "\n\033[0;37m Skipping extensions\033[0m\n"
+        printf "\n${GREY} Skipping extensions${NO_COLOR}\n"
             ;;
         *)
-    printf "\n\033[0;31mInvalid input...\033[0m\n"
+    printf "\n${RED}Invalid input...${NO_COLOR}\n"
     exit 1
     ;;
     esac    
        ;;
     [nN][oO]|[nN])
-    printf "\n\033[0;37m Skipping VSCode\033[0m\n"
+    printf "\n${GREY} Skipping VSCode${NO_COLOR}\n"
        ;;
     *)
- printf "\n\033[0;31mInvalid input...\033[0m\n"
+ printf "\n${RED}Invalid input...${NO_COLOR}\n"
  exit 1
  ;;
 esac
 
 case $install_postman in
     [yY][eE][sS]|[yY])
-    printf "\n\033[0;32mInstalling Postman\033[0m\n"    
+    printf "\n${GREEN}Installing Postman${NO_COLOR}\n"    
     sudo snap install postman
         ;;
     [nN][oO]|[nN])
-    printf "\n\033[0;37m Skipping Postman\033[0m\n"
+    printf "\n${GREY} Skipping Postman${NO_COLOR}\n"
        ;;
     *)
- printf "\n\033[0;31mInvalid input...\033[0m\n"
+ printf "\n${RED}Invalid input...${NO_COLOR}\n"
  exit 1
  ;;
 esac
 
 case $install_mongodb in
     [yY][eE][sS]|[yY])
-    printf "\n\033[0;32mInstalling MongoDB\033[0m\n"    
+    printf "\n${GREEN}Installing MongoDB${NO_COLOR}\n"    
     sudo apt-get install gnupg -y
     wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
     printf "\ndeb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
@@ -219,79 +228,79 @@ case $install_mongodb in
     sudo service mongod start
         ;;
     [nN][oO]|[nN])
-    printf "\n\033[0;37m Skipping MongoDB\033[0m\n"
+    printf "\n${GREY} Skipping MongoDB${NO_COLOR}\n"
        ;;
     *)
- printf "\n\033[0;31mInvalid input...\033[0m\n"
+ printf "\n${RED}Invalid input...${NO_COLOR}\n"
  exit 1
  ;;
 esac
 
 case $install_nodejs in
     [yY][eE][sS]|[yY])
-    printf "\n\033[0;32mInstalling NodeJS\033[0m\n"    
+    printf "\n${GREEN}Installing NodeJS${NO_COLOR}\n"    
     sudo apt install curl -y
     curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
     sudo apt install nodejs -y
         ;;
     [nN][oO]|[nN])
-    printf "\n\033[0;37m Skipping NodeJS\033[0m\n"
+    printf "\n${GREY} Skipping NodeJS${NO_COLOR}\n"
        ;;
     *)
- printf "\n\033[0;31mInvalid input...\033[0m\n"
+ printf "\n${RED}Invalid input...${NO_COLOR}\n"
  exit 1
  ;;
 esac
 
 case $install_vlc in
     [yY][eE][sS]|[yY])
-    printf "\n\033[0;32mInstalling VLC\033[0m\n"    
+    printf "\n${GREEN}Installing VLC${NO_COLOR}\n"    
     sudo snap install vlc
         ;;
     [nN][oO]|[nN])
-    printf "\n\033[0;37m Skipping VLC\033[0m\n"
+    printf "\n${GREY} Skipping VLC${NO_COLOR}\n"
        ;;
     *)
- printf "\n\033[0;31mInvalid input...\033[0m\n"
+ printf "\n${RED}Invalid input...${NO_COLOR}\n"
  exit 1
  ;;
 esac
 
 case $install_terminator in
     [yY][eE][sS]|[yY])
-    printf "\n\033[0;32mInstalling Terminator\033[0m\n"    
+    printf "\n${GREEN}Installing Terminator${NO_COLOR}\n"    
     sudo apt-get install terminator -y
         ;;
     [nN][oO]|[nN])
-    printf "\n\033[0;37m Skipping Terminator\033[0m\n"
+    printf "\n${GREY} Skipping Terminator${NO_COLOR}\n"
        ;;
     *)
- printf "\n\033[0;31mInvalid input...\033[0m\n"
+ printf "\n${RED}Invalid input...${NO_COLOR}\n"
  exit 1
  ;;
 esac
 
 case $install_docker in
     [yY][eE][sS]|[yY])
-    printf "\n\033[0;32mInstalling Docker\033[0m\n"  
+    printf "\n${GREEN}Installing Docker${NO_COLOR}\n"  
     curl -fsSL https://get.docker.com -o get-docker.sh
     sh get-docker.sh
     sudo usermod -aG docker "$USER"
-    printf "\n\033[0;32mTo use Docker as a non-root user, you will have to log out and back in.\033[0m\n"
+    printf "\n${GREEN}To use Docker as a non-root user, you will have to log out and back in.${NO_COLOR}\n"
     rm get-docker.sh
         ;;
     [nN][oO]|[nN])
-    printf "\n\033[0;37m Skipping Docker\033[0m\n"
+    printf "\n${GREY} Skipping Docker${NO_COLOR}\n"
        ;;
     *)
- printf "\n\033[0;31mInvalid input...\033[0m\n"
+ printf "\n${RED}Invalid input...${NO_COLOR}\n"
  exit 1
  ;;
 esac
 
 case $install_docker_compose in
     [yY][eE][sS]|[yY])
-    printf "\n\033[0;32mInstalling Docker Compose\033[0m\n" 
+    printf "\n${GREEN}Installing Docker Compose${NO_COLOR}\n" 
     # source: https://gist.github.com/wdullaer/f1af16bd7e970389bad3 
     COMPOSE_VERSION=$(git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oE "[0-9]+\.[0-9][0-9]+\.[0-9]+$" | sort --version-sort | tail -n 1)
     sudo curl -L "https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
@@ -299,10 +308,10 @@ case $install_docker_compose in
     sudo sh -c "curl -L https://raw.githubusercontent.com/docker/compose/${COMPOSE_VERSION}/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
         ;;
     [nN][oO]|[nN])
-    printf "\n\033[0;37m Skipping Docker Compose\033[0m\n"
+    printf "\n${GREY} Skipping Docker Compose${NO_COLOR}\n"
         ;;
     *)
- printf "\n\033[0;31mInvalid input...\033[0m\n"
+ printf "\n${RED}Invalid input...${NO_COLOR}\n"
  exit 1
  ;;
 esac
